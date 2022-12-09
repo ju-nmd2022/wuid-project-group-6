bcfb181102422a41ef5941762ca243c89a568705
lilo22ga <lilo22ga@student.ju.se>
2022-12-09 14:35:12 +0100

    changed background pic and added font

---
+++ app.js
@@ -0,0 +1,19 @@
+// function subscribe() {
+//  const email = document.querySelector(".p-email").innerHTML;
+// const email = document.querySelector(".p-email").value;
+
+//console.log("subscribing", email);
+//window.localStorage.setItem("email", email);
+// window.location = '/subscription-page.html"';
+//}
+
+//document
+//.querySelector(".smallsubscribe")
+//.addEventListener("onClick", subscribe);
+
+// const email = document.querySelector(".p-email").innerHTML;
+const email = document.querySelector(".p-email").value;
+
+console.log(email)e.log("subscribing", email);
+
+window.localStorage.setItem("email", email);
