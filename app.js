f8875a89599cfa1af294851e9f9bcf4dc277748c
lilo22ga <lilo22ga@student.ju.se>
2022-12-10 17:49:07 +0100

    added second javascript

--- app.js
+++ app.js
@@ -13,3 +13,16 @@ if (insertemail) {
   const email = window.localStorage.getItem("email");
   insertemail.innerHTML = email;
 }
+
+const searchtext = document.querySelector(".text");
+const searchOptions = document.querySelector(".searchoptions");
+
+if (searchtext) {
+  searchtext.addEventListener("input", function () {
+    if (searchtext.value.toLowerCase() === "p") {
+      searchOptions.style.display = "flex";
+    } else {
+      searchOptions.style.display = "none";
+    }
+  });
+}
